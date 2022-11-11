ff24562ce60f809f473dd8a9d51f5b701cabdca4
Mads Obitsø <madsobitsoe@gmail.com>
2022-11-11 11:33:21 +0100

    Ugly code, but I can generate smtlib from ebpf-tools.ASM

--- code/FeBPFVCGen/app/Main.hs
+++ code/FeBPFVCGen/app/Main.hs
@@ -20,17 +20,60 @@ testProg = [ Binary B64 Mov (Reg 0) (Left (Reg 2))
 
 testProgOnlyExit = [ Exit ]
 
+testProgOneMov = [ Binary B64 Mov (Reg 0) (Left (Reg 2))
+                 , Exit ]
+
+testProgTwoMov = [ Binary B64 Mov (Reg 0) (Left (Reg 2))
+                 , Binary B64 Mov (Reg 3) (Right 42)
+                 , Exit ]
+testProgMovAndAdd = [ Binary B64 Mov (Reg 0) (Left (Reg 2))
+                    , Binary B64 Add (Reg 0) (Right 42)
+                    , Exit ]
+
+testProgRegAdd = [ Binary B64 Mov (Reg 0) (Left (Reg 2))
+                 , Binary B64 Add (Reg 0) (Left (Reg 2))
+                 , Exit ]
+
+
+testProgRegDiv = [ Binary B64 Mov (Reg 0) (Right 10)
+                 , Binary B64 Mov (Reg 1) (Left (Reg 2))
+                 , Binary B64 Div (Reg 0) (Left (Reg 1))
+                 , Exit ]
+
 
 main :: IO ()
 main =
   do
-    case genVC testProgOnlyExit of
+    -- case genVC testProgOnlyExit of
+    --   Left err -> print err
+    --   Right res -> print res
+    -- case genVC testProg of
+    --   Left err -> print err
+    --   Right res -> print res
+    -- putStrLn "One mov case:"
+    -- case genVC testProgOneMov of
+    --   Left err -> print err
+    --   Right res -> print res
+    -- putStrLn "Two mov case:"
+    -- case genVC testProgTwoMov of
+    --   Left err -> print err
+    --   Right res -> print res
+    -- putStrLn "Mov and Add case:"      
+    -- case genVC testProgMovAndAdd of
+    --   Left err -> print err
+    --   Right (Judgment vc ms cs) -> do
+    --     putStrLn "Constants:"
+    --     print cs
+    --     putStrLn "VC:"
+    --     print vc
+
+    case genVC testProgRegDiv of
       Left err -> print err
-      Right res -> print $ S.pp res
-    case genVC testProg of
-      Left err -> print err
-      Right res -> print $ S.pp res
-        -- progRes = eval []
-    -- print $ t
-    -- print $ getConstants testProg
-    -- putStrLn "Woop woop"
+      Right vc -> do
+          let (Script cs) = judgmentToConstantDeclarations vc
+              (Script vc') = judgmentToAssertion vc 
+            in print $ pp $ withDefaults (Script $ cs ++ vc')
+
+
+
+
--- code/FeBPFVCGen/src/VCGen.hs
+++ code/FeBPFVCGen/src/VCGen.hs
@@ -1,12 +1,12 @@
 module VCGen
-    ( genVC, withDefaultOptions
+    ( genVC, withDefaultOptions, Judgment(..), judgmentToConstantDeclarations, judgmentToAssertion, withDefaults
     ) where
 
 import Ebpf.Asm as A
 import Ebpf.Decode as D
 import SMTLib2 as S
 import SMTLib2.Core as SC
-import SMTLib2.BitVector as SBV
+import qualified SMTLib2.BitVector as SBV
 import Data.Maybe
 import Data.Word
 import qualified Data.Map.Strict as M
@@ -20,13 +20,13 @@ data Uninit = U0 | U3 | U4 | U5 | U6 | U7 | U8 | U9
   deriving (Ord, Show, Eq)
   
 data SExpr =
-    Imm Word64
-  | Var VName
-  | UnVar Uninit
-  | Add SExpr SExpr
-  | Sub SExpr SExpr
-  | Div SExpr SExpr
-  | Xor SExpr SExpr
+    SImm Word64
+  | SVar VName
+  | SUnVar Uninit
+  | SAdd SExpr SExpr
+  | SSub SExpr SExpr
+  | SDiv SExpr SExpr
+  | SXor SExpr SExpr
   deriving (Show)
 
 data PSExpr =
@@ -35,41 +35,59 @@ data PSExpr =
   | Eq SExpr SExpr
   | Neq SExpr SExpr
   | Geq SExpr SExpr
-  | LT SExpr SExpr
+  | Lt SExpr SExpr
   -- | Init SExpr
   deriving (Show)
 
 data Pred =
-    PSExpr
+    PS PSExpr
+  | Not Pred 
   | LAnd Pred Pred
   | Impl Pred Pred
   deriving (Show)
   -- | FAll 
 
 
+-- Add linenumbers to each instruction in a program
+numberInstrs = zip [1..]
 
-pred2VC :: Pred -> VC
--- pred2VC (Eq (Var n) (Imm i)) =  S.Script [ (SC.=== (funRef n) (bv (fromIntegral i) 64)) ]
-pred2VC _ = undefined
+
 
 type VC = S.Script
 
 type VCGenError = String
 type MachineState = M.Map Register SExpr
 
-initialMachineState = M.fromList [ (R0, UnVar U0)
-                                 , (R1, Var "m")
-                                 , (R2, Var "n")
-                                 , (R3, UnVar U3)
-                                 , (R4, UnVar U4)
-                                 , (R5, UnVar U5)
-                                 , (R6, UnVar U6)
-                                 , (R7, UnVar U7)
-                                 , (R8, UnVar U8)
-                                 , (R9, UnVar U9)
-                                 , (R10, Var "fp")
+type Constants = M.Map Register [VName]
+
+initialConstants = M.fromList [ (R0, ["r0_0"])
+                              , (R1, ["m"])
+                              , (R2, ["n"])
+                              , (R3, ["r3_0"])
+                              , (R4, ["r4_0"])
+                              , (R5, ["r5_0"])
+                              , (R6, ["r6_0"])
+                              , (R7, ["r7_0"])
+                              , (R8, ["r8_0"])
+                              , (R9, ["r9_0"])
+                              , (R10,["fp"])
+                              ]
+
+
+initialMachineState = M.fromList [ (R0, SUnVar U0)
+                                 , (R1, SVar "m")
+                                 , (R2, SVar "n")
+                                 , (R3, SUnVar U3)
+                                 , (R4, SUnVar U4)
+                                 , (R5, SUnVar U5)
+                                 , (R6, SUnVar U6)
+                                 , (R7, SUnVar U7)
+                                 , (R8, SUnVar U8)
+                                 , (R9, SUnVar U9)
+                                 , (R10, SVar "fp")
                                  ]
 
+-- Helper function to convert between ebpf-tools registers and VCGen registers
 reg2reg (A.Reg 0) = R0
 reg2reg (A.Reg 1) = R1
 reg2reg (A.Reg 2) = R2
@@ -83,43 +101,120 @@ reg2reg (A.Reg 9) = R9
 reg2reg (A.Reg 10) = R10
 reg2reg _ = undefined
 
-data Judgment = Judgment VC MachineState
+data Judgment = Judgment Pred MachineState Constants
+  deriving (Show)
 
-genVC :: A.Program -> Either VCGenError VC
-genVC prog =
-  case innerGenVC ((Judgment (S.Script []) initialMachineState)) prog of
-    Left err -> Left err
-    Right (Judgment vc ms) -> Right $ withDefaultOptions $ withDefaultPreconditions $ withDefaultPostamble vc
+genVC :: A.Program -> Either VCGenError Judgment
+genVC prog = innerGenVC initialJudgment (numberInstrs prog)
 
-innerGenVC :: Judgment -> A.Program -> Either VCGenError Judgment
+
+
+-- Given two predicates where the first is specifically an implication,
+-- Chain them such that the conclusion of the implication implies the new term
+addImpl :: Pred -> Pred -> Pred
+-- If we have true => P as the first term, and Q as second term, make it P => Q
+addImpl (Impl (PS PTrue) p) q = Impl p q
+-- If we have P => true as the first term and Q as the second term, make it P => Q
+addImpl (Impl p (PS PTrue)) q = Impl p q
+-- If we have P => (Q => R) and S, make it P => (Q => (R => S))
+addImpl (Impl p (Impl q r)) s = Impl p (Impl q (Impl r s))
+-- If we have P -> Q as first term and R as second, make it P -> (Q -> R) by calling recursively
+addImpl (Impl p q) r = Impl p (Impl q r)
+addImpl p q = Impl p q
+addImpl _ _ = undefined
+
+innerGenVC :: Judgment -> [LineNoInstruction] -> Either VCGenError Judgment
 innerGenVC judgment [] = Left "Invalid program: Program was empty before exit"
-innerGenVC judgment [A.Exit] = genVC' judgment A.Exit
+innerGenVC judgment [(_,A.Exit)] = genVC' judgment (0, A.Exit)
 innerGenVC judgment (i:is) =
   case genVC' judgment i of
     Left err -> Left err
     Right judgment' -> innerGenVC judgment' is
   
 
-initialJudgment = Judgment (S.Script []) initialMachineState
-
-updateVC :: S.Script -> PSExpr -> S.Script
-updateVC _ _ = undefined
+initialJudgment = Judgment (Impl (PS (Geq (SVar "n") (SImm 1))) (PS PTrue)) initialMachineState initialConstants
 
 
-genVC' :: Judgment -> A.Instruction -> Either VCGenError Judgment
-genVC' (Judgment vc ms) (A.Exit) =
+type LineNoInstruction = (Int, A.Instruction)
+
+
+genVC' :: Judgment -> LineNoInstruction -> Either VCGenError Judgment
+genVC' (Judgment vc ms cs) (lineno, A.Exit) =
   case M.lookup R0 ms of
     Nothing -> Left "INTERNAL ERROR: R0 not in map"
-    Just (UnVar _) -> Left "Invalid Program: R0 not initialized at exit"
-    Just _ -> Right (Judgment vc ms)
+    Just (SUnVar _) -> Left "Invalid Program: R0 not initialized at exit"
+    Just _ -> Right (Judgment vc ms cs)
       
-genVC' (Judgment vc ms) (A.Binary A.B64 A.Mov (A.Reg d) (Left (A.Reg s)))  =
+genVC' (Judgment vc ms cs) (lineno, A.Binary A.B64 A.Mov (A.Reg d) (Left (A.Reg s)))  =
                          let rd = reg2reg (A.Reg d)
                              rs = reg2reg (A.Reg s)
                          in case M.lookup rs ms of
                            Nothing -> Left "Key not in map"
-                           Just sexp -> Right $ Judgment vc (M.insert rd sexp ms) 
+                           Just sexp ->
+                             case M.lookup rd cs of
+                               Nothing -> Left "INTERNAL ERROR"
+                               Just consts ->
+                                 let newConst = (show rd ++ "_" ++ show lineno)
+                                     newConsts = M.insert rd (newConst:consts) cs
+                                 in
+                                   Right $ Judgment (addImpl vc (PS (Eq (SVar newConst) sexp))) (M.insert rd sexp ms) newConsts
 
+genVC' (Judgment vc ms cs) (lineno, A.Binary A.B64 A.Mov (A.Reg d) (Right n))  =
+  let rd = reg2reg (A.Reg d)
+  in
+    case M.lookup rd cs of
+      Nothing -> Left "INTERNAL ERROR"
+      Just consts ->
+        let newConst = (show rd ++ "_" ++ show lineno)
+            newConsts = M.insert rd (newConst:consts) cs
+        in Right $ Judgment (addImpl vc (PS (Eq (SVar newConst) (SImm (fromIntegral n))))) (M.insert (reg2reg (A.Reg d)) (SImm (fromIntegral n)) ms) newConsts 
+
+genVC' (Judgment vc ms cs) (lineno, A.Binary A.B64 A.Add (A.Reg d) (Right n))  =
+  let rd = reg2reg (A.Reg d)
+  in
+    case M.lookup rd cs of
+      Nothing -> Left "INTERNAL ERROR"
+      Just consts ->
+        let newConst = (show rd ++ "_" ++ show lineno)
+            newConsts = M.insert rd (newConst:consts) cs
+        in Right $ Judgment (addImpl vc (PS (Eq (SVar newConst) (SAdd (SVar (head consts)) (SImm (fromIntegral n)))))) (M.insert (reg2reg (A.Reg d)) (SAdd (SVar newConst) (SImm (fromIntegral n))) ms) newConsts
+
+genVC' (Judgment vc ms cs) (lineno, A.Binary A.B64 A.Add (A.Reg d) (Left (Reg s)))  =
+  let rd = reg2reg (A.Reg d)
+      rs = reg2reg (A.Reg s)
+  in case M.lookup rs ms of
+    Nothing -> Left "Key not in map"
+    Just sexp ->
+      case M.lookup rs cs of
+        Nothing -> Left "INTERNAL ERROR"
+        Just srcConsts ->
+          case M.lookup rd cs of
+            Nothing -> Left "INTERNAL ERROR"
+            Just dstConsts ->
+              let newConst = (show rd ++ "_" ++ show lineno)
+                  newConsts = M.insert rd (newConst:dstConsts) cs
+              in Right $ Judgment (addImpl vc (PS (Eq (SVar newConst) (SAdd (SVar (head dstConsts)) (SVar (head srcConsts)))))) (M.insert (reg2reg (A.Reg d)) (SAdd (SVar newConst) (SVar (head srcConsts))) ms) newConsts
+
+
+genVC' (Judgment vc ms cs) (lineno, A.Binary A.B64 A.Div (A.Reg d) (Left (Reg s)))  =
+  let rd = reg2reg (A.Reg d)
+      rs = reg2reg (A.Reg s)
+  in case M.lookup rs ms of
+    Nothing -> Left "Key not in map"
+    Just sexp ->
+      case M.lookup rs cs of
+        Nothing -> Left "INTERNAL ERROR"
+        Just srcConsts ->
+          case M.lookup rd cs of
+            Nothing -> Left "INTERNAL ERROR"
+            Just dstConsts ->
+              let newConst = (show rd ++ "_" ++ show lineno)
+                  newConsts = M.insert rd (newConst:dstConsts) cs
+                  notZeroPred = PS (Neq (SVar (head srcConsts)) (SImm 0))
+                  postDivPred = PS (Eq (SVar newConst) (SDiv (SVar (head dstConsts)) (SVar (head srcConsts))))
+              in
+                Right $ Judgment (addImpl vc (addImpl notZeroPred postDivPred)) (M.insert rd (SDiv (SVar (head dstConsts)) (SVar (head srcConsts))) ms) newConsts
+                
 genVC' _ _ = Left "Not implemented yet"
 
 
@@ -131,11 +226,12 @@ withDefaultOptions (S.Script cmds) =
            , CmdSetOption $ OptAttr (Attr (N "proof-format-mode lfsc") Nothing)
            , CmdSetOption $ OptAttr (Attr (N "dump-proofs true") Nothing)] ++ cmds
 
-withDefaultPreconditions (S.Script cmds) =
-  S.Script $ [ declFun64 "m", declFun64 "n" ] ++ cmds
+
 
 withDefaultPostamble (S.Script cmds) =
   S.Script $ cmds ++ [ CmdCheckSat, CmdExit ]
+
+withDefaults = withDefaultOptions . withDefaultPostamble
 -- getConstDecl :: (Int,A.Instruction)  -> [S.Command] -> [S.Command]
 -- getConstDecl (line, Binary B32 _  (Reg regname) _) acc =  (declFun32 $ "r" ++ show regname ++ "_" ++ show line) : acc
 -- getConstDecl (line, Binary B64 _  (Reg regname) _) acc =  (declFun64 $ "r" ++ show regname ++ "_" ++ show line) : acc
@@ -150,11 +246,9 @@ withDefaultPostamble (S.Script cmds) =
 
 
 
--- -- withDefaultPreconditions (Script cmds) =
--- --   Script $ [ 
 
 -- declFun32 name = CmdDeclareFun (N name) [] (tBitVec 32)
-declFun64 name = CmdDeclareFun (N name) [] (tBitVec 64)
+declFun64 name = CmdDeclareFun (N name) [] (SBV.tBitVec 64)
 funRef name = App (I (N name) []) Nothing []
 
 -- -- "good_div.smt2" program
@@ -166,7 +260,76 @@ funRef name = App (I (N name) []) Nothing []
 --                         ]
 
 
--- numberInstrs = zip [1..]
 
--- -- getConstants :: A.Program -> Doc
--- getConstants prog = pp $ withDefaultOptions $ getConstDecls prog
+judgmentToConstantDeclarations :: Judgment -> S.Script
+judgmentToConstantDeclarations (Judgment _ _ cs) =
+  S.Script $ map declFun64 $ concat $ M.elems cs
+
+
+sexprToScript :: SExpr -> S.Expr
+sexprToScript (SImm w) = SBV.bv (fromIntegral w) 64
+sexprToScript (SVar name) = funRef name
+sexprToScript (SUnVar uninitReg) = undefined
+sexprToScript (SAdd s1 s2) =
+  let s1' = sexprToScript s1
+      s2' = sexprToScript s2
+  in
+    SBV.bvadd s1' s2'
+
+sexprToScript (SSub s1 s2) =
+  let s1' = sexprToScript s1
+      s2' = sexprToScript s2
+  in
+    SBV.bvsub s1' s2'
+
+sexprToScript (SDiv s1 s2) =
+  let s1' = sexprToScript s1
+      s2' = sexprToScript s2
+  in
+    SBV.bvudiv s1' s2'
+  
+sexprToScript (SXor s1 s2) =
+  let s1' = sexprToScript s1
+      s2' = sexprToScript s2
+  in
+    SBV.bvxor s1' s2'
+
+psExprToScript :: PSExpr -> S.Expr
+psExprToScript PTrue = SC.true --undefined
+psExprToScript PFalse = SC.false -- undefined
+psExprToScript (Eq s1 s2) =
+  let s1' = sexprToScript s1
+      s2' = sexprToScript s2
+  in
+    (SC.===) s1' s2'
+
+psExprToScript (Neq s1 s2) =
+  SC.not $ psExprToScript (Eq s1 s2)
+
+psExprToScript (Geq s1 s2) =
+  let s1' = sexprToScript s1
+      s2' = sexprToScript s2
+  in
+    SBV.bvuge s1' s2'
+psExprToScript (Lt s1 s2) =
+  let s1' = sexprToScript s1
+      s2' = sexprToScript s2
+  in
+    SBV.bvult s1' s2'
+
+predToExpr :: Pred -> S.Expr
+predToExpr (PS psexpr) = psExprToScript psexpr
+predToExpr (Not pred) = SC.not $ predToExpr pred
+predToExpr (LAnd p1 p2) =
+  let p1' = predToExpr p1
+      p2' = predToExpr p2
+  in
+    SC.and p1' p2'
+predToExpr (Impl p1 p2) =
+  let p1' = predToExpr p1
+      p2' = predToExpr p2
+  in
+    (SC.==>) p1' p2'
+
+judgmentToAssertion :: Judgment -> S.Script
+judgmentToAssertion (Judgment p _ _) = S.Script [S.CmdAssert (SC.not $ predToExpr p)] 
